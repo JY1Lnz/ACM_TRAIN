4bbcbfb14ead85f65afc253a8c92aec318bfb2cf
晋毓麟 <2522471070@qq.com>
2018-12-14 12:50:33 +0800

    回路遍历

--- test.cpp
+++ test.cpp
@@ -1,24 +1,53 @@
-#include<bits/stdc++.h>
+#include<iostream>
+#include<cstdio>
+#include<cstring>
 using namespace std;
-struct Node
-{
-    int num;
-    Node * left;
-    Node * right;
-    Node():num(0),left(NULL),right(NULL){};
-};
+int G[110][110],n,m,a,b;
+int vis[110],c[110], topo[110], t;
 
-Node * newnode() {return new Node();}
-
-int main()
-{
-    string s = "1 2 3";
-    istringstream iss(s);
-    int a;
-    while(iss>>a)
-    {
-        cout<<a<<endl;
+bool dfs(int u){
+    cout<<u<<endl;
+    vis[u] = -1; //表示正在访问
+    for(int v=1; v<=n; ++v) if(G[u][v]){
+            cout<<u<<' '<<v<<' '<<vis[v]<<endl;
+        if(vis[v] == -1) return false; // 如果存在有向环，失败退出
+        else if(!vis[v] && !dfs(v)) return false;
     }
+    // 结束访问
+    printf("%d",topo[0]);
+            for(int i=1; i<n; ++i)
+                printf(" %d",topo[i]);
+            printf("\n");
+    vis[u] = 1; topo[--t] = u;
+    return true;
+}
 
+bool topoSort(){
+    t = n;
+    memset(vis, 0, sizeof(vis));
+    for(int u=1; u<=n; ++u)
+        if(!vis[u] && !dfs(u)) return false;
+    return true;
+}
+
+int main(){
+#ifdef LOCAL
+    freopen("input.txt","r",stdin);
+#endif
+    // 注意输入那里的结束条件不能是 n&&m,因为m可能是0
+    while(~scanf("%d %d",&n,&m) && n+m){
+        memset(G, 0, sizeof(G));
+        for(int i=0; i<m; ++i){
+            scanf("%d %d",&a,&b);
+            G[a][b] = 1;
+        }
+        if(topoSort()) {
+            printf("%d",topo[0]);
+            for(int i=1; i<n; ++i)
+                printf(" %d",topo[i]);
+            printf("\n");
+        }
+    }
     return 0;
 }
+
---
+++ 欧拉回路.cpp
@@ -0,0 +1,39 @@
+#include<bits/stdc++.h>
+using namespace std;
+const int maxn = 10;
+int data[maxn][maxn];
+int vis[maxn][maxn];
+stack<pair<int,int> > path;
+void euler(int u)
+{
+    for (int v = 0;v < maxn;v++)
+        if (vis[u][v] && !data[u][v])
+        {
+            data[u][v] = data[v][u] = 1;
+            euler(v);
+            path.push(make_pair(u,v));
+        }
+}
+
+int main()
+{
+    int m;
+    while(cin>>m)
+    {
+        int x,y;
+        for (int i = 0;i<m;i++)
+        {
+            cin>>x>>y;
+            vis[x][y] = vis[y][x] = 1;
+        }
+
+        euler(1);
+        while(!path.empty())
+        {
+            cout<<path.top().first<<' '<<path.top().second<<endl;
+            path.pop();
+        }
+    }
+
+    return 0;
+}
